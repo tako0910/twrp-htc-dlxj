92be987a6b516a6395b1e7527c9a0fc36f90eead
tako0910 <tako.oct0910@gmail.com>
2015-03-07 22:53:30 +0900

    fix kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlxj user
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := cyanogenmod_dlxj_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/m7
+TARGET_KERNEL_SOURCE := kernel/htc/m7wlj
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
